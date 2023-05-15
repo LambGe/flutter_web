53c3adf62879641c4a33fb7101bdef9ab4cf8d40
LambGe <119394755+LambGe@users.noreply.github.com>
2023-05-15 15:20:42 +0800

    test code again

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    String test = "test code";
     setState(() {
       _counter++;
     });
